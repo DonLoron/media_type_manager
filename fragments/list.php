b821549f6c31dfd57db1553bb89afbd72c96d4f2
donLoron <lw@walderwerber.ch>
2018-04-10 10:15:16 +0200

    updated list ui

--- fragments/list.php
+++ fragments/list.php
@@ -1,15 +1,32 @@
 <?php
 /* @var $this rex_fragment */
 ?>
-Media Set Typen<br>
-<?
-foreach($this->listElements as $element) {
-  ?>
-  <br>
-  <a href="<?= rex_url::currentBackendPage(["func" => "edit", "mediatypeSetName" => $element['mediatypeSetName']]) ?>"><?= $element['mediatypeSetName'] ?></a><br>
-  <?
-}
-?>
-<br>
-<a class="btn btn-edit" href="<?= rex_url::currentBackendPage(["func" => "add"]) ?>">Neues Set hinzufügen</a>
-
+<div class="panel panel-default">
+  <div class="panel-heading">Media Set Typen</div>
+  <table class="panel-body table table-striped">
+    <thead>
+      <tr>
+        <th>Media Typ Name</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?
+    foreach($this->listElements as $element) {
+      ?>
+      <tr>
+        <td><a href="<?= rex_url::currentBackendPage(["func" => "edit", "mediatypeSetName" => $element['mediatypeSetName']]) ?>"><?= $element['mediatypeSetName'] ?></a>
+          <div class="btn-group pull-right">
+            <a class="btn btn-xs btn-default" href="<?= rex_url::currentBackendPage(["func" => "edit", "mediatypeSetName" => $element['mediatypeSetName']]) ?>"><span class="glyphicon glyphicon-edit"></span></a>
+            <a class="btn btn-xs btn-danger" href="<?= rex_url::currentBackendPage(["func" => "delete", "mediatypeSetName" => $element['mediatypeSetName']]) ?>"><span class="glyphicon glyphicon-trash"></span></a>
+          </div>
+        </td>
+      </tr>
+      <?
+    }
+    ?>
+    </tbody>
+  </table>
+  <div class="panel-footer">
+    <a class="btn btn-edit" href="<?= rex_url::currentBackendPage(["func" => "add"]) ?>">Neues Set hinzufügen</a>
+  </div>
+</div>
