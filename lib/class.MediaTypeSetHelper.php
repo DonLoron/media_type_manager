3aa2bae2e8f1ba6fa80166f1bac8e0895a38447f
donLoron <lw@walderwerber.ch>
2018-04-10 11:02:23 +0200

    added static HTML Picture Method (for starters)

--- lib/class.MediaTypeSetHelper.php
+++ lib/class.MediaTypeSetHelper.php
@@ -16,6 +16,46 @@ class MediaTypeSetHelper
     return $effects;
   }
 
+  public static function getPictureTag ($mediaTypeSetName, $file) {
+
+    $mediaTypeSet = MediaTypeSet::getSetByName($mediaTypeSetName);
+
+    if(!is_array($mediaTypeSet)) return false;
+
+    $media = rex_media::get($file);
+    if($media instanceof rex_media) $title = addslashes($media->getTitle());
+    else $title = " ";
+
+    $tag = '';
+    $tag .= '<picture>'.PHP_EOL;
+
+    foreach($mediaTypeSet['breakpoints'] as $breakpoint) {
+
+      //TODO: Add values in media manager form per breakpoint
+      switch($breakpoint['breakpointName']) {
+        case 'L':
+          $mediaQuery = "(min-width: 1025px)";
+          break;
+        case 'M':
+          $mediaQuery = "(min-width: 751px) and (max-width: 1024px)";
+          break;
+        case 'S':
+          $mediaQuery = "(min-width: 376px) and (max-width: 750px)";
+          break;
+        case 'XS':
+          $mediaQuery = "(max-width: 375px)";
+          break;
+      }
+
+      $tag .= '	<source media="' . $mediaQuery . '" srcset="index.php?rex_media_type='.$mediaTypeSetName.'-' . $breakpoint['breakpointName'] . '@1x&rex_media_file='.$file.' 1x, index.php?rex_media_type='.$mediaTypeSetName.'-' . $breakpoint['breakpointName'] . '@2x&rex_media_file='.$file.' 2x">'.PHP_EOL;
+    }
+
+    $tag .= '	<img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" alt="' . $title .'">'.PHP_EOL;
+    $tag .= '</picture>'.PHP_EOL;
+
+    return $tag;
+  }
+
   public static function handleAJAX($action) {
 
     $response = [];
